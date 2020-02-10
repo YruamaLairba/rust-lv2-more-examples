a70c29e04767de122adef498b7dac231cb3f971a
Yruama_Lairba <yruama_lairba@hotmail.com>
2020-02-10 18:05:49 +0100

    write an extern C work function

--- worker/src/lib.rs
+++ worker/src/lib.rs
@@ -1,4 +1,10 @@
 use lv2_core::prelude::*;
+use lv2_sys::{
+    LV2_Handle, LV2_Worker_Respond_Function, LV2_Worker_Respond_Handle, LV2_Worker_Status,
+    LV2_Worker_Status_LV2_WORKER_ERR_NO_SPACE, LV2_Worker_Status_LV2_WORKER_ERR_UNKNOWN,
+    LV2_Worker_Status_LV2_WORKER_SUCCESS,
+};
+use std::os::raw::*; //get all commoent c_type
 
 // see lv2_core::port::PortContainer for port type
 #[derive(PortContainer)]
@@ -23,7 +29,6 @@ impl Plugin for EgWorker {
         Some(Self)
     }
 
-
     fn activate(&mut self) {}
 
     fn deactivate(&mut self) {}
@@ -39,6 +44,18 @@ impl Plugin for EgWorker {
             *out_frame = in_frame * coef;
         }
     }
+}
+
+// Worker spec implementation
+
+extern "C" fn work(
+    _instance: LV2_Handle,
+    _respond: LV2_Worker_Respond_Function,
+    _handle: LV2_Worker_Respond_Handle,
+    _size: u32,
+    _data: *const c_void,
+) -> LV2_Worker_Status {
+    return LV2_Worker_Status_LV2_WORKER_SUCCESS;
 }
 
 lv2_descriptors!(EgWorker);

2cd079e30719bdf997ee6fab491ca31f78c52811
Yruama_Lairba <yruama_lairba@hotmail.com>
2020-02-16 16:06:18 +0100

    Move many thing inside extern lv2_worker crate

--- worker/src/lib.rs
+++ worker/src/lib.rs
@@ -1,13 +1,26 @@
 use core::any::Any;
 use lv2_core::extension::ExtensionDescriptor;
+use lv2_core::feature::*;
 use lv2_core::prelude::*;
+use lv2_urid::prelude::*;
+use lv2_sys;
 use lv2_sys::{
-    LV2_Handle, LV2_WORKER__interface, LV2_Worker_Interface, LV2_Worker_Respond_Function,
-    LV2_Worker_Respond_Handle, LV2_Worker_Status, LV2_Worker_Status_LV2_WORKER_ERR_NO_SPACE,
-    LV2_Worker_Status_LV2_WORKER_ERR_UNKNOWN, LV2_Worker_Status_LV2_WORKER_SUCCESS,
+    LV2_Handle,
+    LV2_WORKER__interface,
+    LV2_Worker_Interface,
+    LV2_Worker_Respond_Function,
+    LV2_Worker_Respond_Handle,
+    LV2_Worker_Schedule,
+    LV2_Worker_Status,
+    LV2_Worker_Status_LV2_WORKER_ERR_NO_SPACE,
+    LV2_Worker_Status_LV2_WORKER_ERR_UNKNOWN,
+    LV2_Worker_Status_LV2_WORKER_SUCCESS,
 };
+use lv2_worker::*;
 use std::marker::PhantomData;
-use std::os::raw::*; //get all commoent c_type
+use std::os::raw::*; //get all common c_type
+
+
 
 // see lv2_core::port::PortContainer for port type
 #[derive(PortContainer)]
@@ -17,7 +30,17 @@ struct Ports {
     output: OutputPort<Audio>,
 }
 
-struct EgWorker;
+#[derive(FeatureCollection)]
+pub struct Features<'a> {
+    map: Map<'a>,
+    schedule: Schedule<'a>,
+}
+
+#[repr(C)]
+struct EgWorker
+{
+    schedule_work: unsafe extern "C" fn(handle: lv2_sys::LV2_Worker_Schedule_Handle, size: u32, data: *const c_void) -> LV2_Worker_Status,
+}
 
 // URI identifier
 unsafe impl UriBound for EgWorker {
@@ -26,10 +49,23 @@ unsafe impl UriBound for EgWorker {
 
 impl Plugin for EgWorker {
     type Ports = Ports;
-    type Features = ();
+    type Features = Features<'static>;
 
-    fn new(_plugin_info: &PluginInfo, _features: ()) -> Option<Self> {
-        Some(Self)
+    fn new(_plugin_info: &PluginInfo, features: Features<'static>) -> Option<Self> {
+        //match features.map.map_type::<Schedule>() {
+        //    Some(x) => println!("Schedule feature {:?}",x),
+        //    None => println!("No Schedule feature"),
+        //}
+        let schedule_work = match features.schedule.internal.schedule_work {
+            Some(s_w) => s_w,
+            None => {
+                println!("Invalid schedule_work pointer");
+                return None;
+            }
+        };
+        Some(Self {
+            schedule_work: schedule_work,
+        })
     }
 
     fn activate(&mut self) {}
@@ -37,6 +73,9 @@ impl Plugin for EgWorker {
     fn deactivate(&mut self) {}
 
     fn run(&mut self, ports: &mut Ports) {
+        unsafe {
+            (self.schedule_work)((self as *mut Self) as *mut std::ffi::c_void, 0, std::ptr::null::<c_void> as *const std::ffi::c_void);
+        }
         let coef = if *(ports.gain) > -90.0 {
             10.0_f32.powf(*(ports.gain) * 0.05)
         } else {
@@ -53,74 +92,6 @@ impl Plugin for EgWorker {
     }
 }
 
-//type
-pub enum WorkerStatus {
-    Success,
-    Unknown,
-    NoSpace,
-}
-
-// Worker Traits
-pub trait Worker: Plugin {
-    fn work(
-        &mut self,
-        response_function: LV2_Worker_Respond_Function,
-        respond_handle: LV2_Worker_Respond_Handle,
-        size: u32,
-        data: *const c_void,
-    ) -> WorkerStatus;
-}
-
-// A descriptor for the plugin. This is just a marker type to associate constants and methods with.
-pub struct WorkerDescriptor<P: Worker> {
-    plugin: PhantomData<P>,
-}
-
-#[repr(C)]
-/// This struct would be part of a sys crate.
-pub struct WorkerInterface {
-    work: unsafe extern "C" fn(
-        LV2_Handle,
-        LV2_Worker_Respond_Function,
-        LV2_Worker_Respond_Handle,
-        u32,
-        *const c_void,
-    ) -> LV2_Worker_Status,
-}
-
-unsafe impl<P: Worker> UriBound for WorkerDescriptor<P> {
-    const URI: &'static [u8] = LV2_WORKER__interface;
-}
-
-impl<P: Worker> WorkerDescriptor<P> {
-    /// The extern, unsafe version of the extending method.
-    ///
-    /// This is actually called by the host.
-    unsafe extern "C" fn extern_work(
-        handle: LV2_Handle,
-        response_function: LV2_Worker_Respond_Function,
-        respond_handle: LV2_Worker_Respond_Handle,
-        size: u32,
-        data: *const c_void,
-    ) -> LV2_Worker_Status {
-        let plugin = (handle as *mut P).as_mut().unwrap();
-        match plugin.work(response_function, respond_handle, size, data) {
-            WorkerStatus::Success => LV2_Worker_Status_LV2_WORKER_SUCCESS,
-            WorkerStatus::Unknown => LV2_Worker_Status_LV2_WORKER_ERR_UNKNOWN,
-            WorkerStatus::NoSpace => LV2_Worker_Status_LV2_WORKER_ERR_NO_SPACE,
-        }
-    }
-}
-
-// Implementing the trait that contains the interface.
-impl<P: Worker> ExtensionDescriptor for WorkerDescriptor<P> {
-    type ExtensionInterface = WorkerInterface;
-
-    const INTERFACE: &'static WorkerInterface = &WorkerInterface {
-        work: Self::extern_work,
-    };
-}
-
 // Actually implementing the extension.
 impl Worker for EgWorker {
     fn work(
@@ -130,6 +101,7 @@ impl Worker for EgWorker {
         _size: u32,
         _data: *const c_void,
     ) -> WorkerStatus {
+        println!("worker thread");
         return WorkerStatus::Success;
     }
 }

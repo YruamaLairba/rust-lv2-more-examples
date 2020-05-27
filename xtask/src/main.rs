c6ac8e2a0d27db75671eb22152b5fb8a70859ffa
Yruama_Lairba <yruama_lairba@hotmail.com>
2020-05-28 00:39:31 +0200

    it works

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,4 +1,6 @@
 #![allow(clippy::try_err)]
+extern crate getopts;
+use getopts::Options;
 use std::env;
 use std::fs;
 use std::fs::File;
@@ -12,147 +14,124 @@ use std::process::Command;
 
 type DynError = Box<dyn std::error::Error>;
 
-#[derive(Clone)]
-struct PackageConf {
-    name: String,
-    dir: String,                 //relative from the workspace root
-    template_files: Vec<String>, //path relative to package dir
-    template_subs: Vec<(String, String)>,
+#[derive(Clone, Copy)]
+struct PackageConf<'a> {
+    name: &'a str,
+    dir: &'a str,                            //relative from the workspace root
+    prefix_tag: &'a str,                     //tag used for prefix replacement
+    suffix_tag: &'a str,                     //tag used for suffix replacement
+    template_files: &'a [&'a str],           //path relative to package dir
+    template_subs: &'a [(&'a str, &'a str)], //additionnal tag/value
+    resources: &'a [&'a str],                //additionnal file to copy into lv2 folder
 }
 
-impl PackageConf {
-    fn with_name(name: &str) -> Self {
-        Self {
-            name: String::from(name),
-            dir: String::from(name),
-            template_files: vec![],
-            template_subs: vec![],
-        }
-    }
+const PACKAGES_CONF: &[PackageConf] = &[PackageConf {
+    name: "eg-worker-rs",
+    dir: "eg-worker-rs",
+    prefix_tag: "@LIB_PREFIX@",
+    suffix_tag: "@LIB_SUFFIX@",
+    template_files: &["manifest.ttl"],
+    template_subs: &[],
+    resources: &["worker.ttl"],
+}];
 
-    fn add_template_file(&mut self, file_name: &str) {
-        self.template_files.push(String::from(file_name));
-    }
-
-    fn add_template_sub(&mut self, token: &str, value: &str) {
-        self.template_subs
-            .push((String::from(token), String::from(value)));
-    }
-}
-
-struct Config {
-    subcommand: Option<String>,
-    target: Option<String>,     // target-triple
-    target_dir: Option<String>, // directory for all generated artifact
+struct Config<'a> {
+    subcommand: String,
+    target: String,     // target-triple
+    target_dir: String, // directory for all generated artifact
     release: bool,
-    packages: Vec<String>,
-    //i didn't find better place to store it
-    packages_conf: Vec<PackageConf>,
+    packages_conf: Vec<PackageConf<'a>>,
+    opts: Options,
 }
 
-impl Config {
-    fn _new() -> Self {
-        Self {
-            subcommand: None,
-            target: None,
-            target_dir: None,
-            release: false,
-            packages: vec![],
-            packages_conf: vec![],
-        }
-    }
-
-    //build config from environment variables
-    fn _vars(mut self) -> Result<Self, DynError> {
-        //let vars = env::vars();
-        //for (key, value) in vars {
-        //    match key {}
-        //}
-        Ok(self)
-    }
-
-    fn _arg_package(&mut self, p: Option<String>) -> Result<(), DynError> {
-        if let Some(p) = p {
-            self.packages.push(p);
-        } else {
-            return Err("The argument '--package' requires a value".into());
-        }
-        Ok(())
-    }
-
-    fn _arg_target(&mut self, tg: Option<String>) -> Result<(), DynError> {
-        if self.target.is_some() {
-            return Err("The argument '--target' was provided more than once".into());
-        }
-        if tg.is_some() {
-            self.target = tg;
-        } else {
-            return Err("The argument '--target' require a value".into());
-        }
-        Ok(())
-    }
-
-    fn _arg_target_dir(&mut self, tg: Option<String>) -> Result<(), DynError> {
-        if self.target_dir.is_some() {
-            return Err("The argument '--target-dir' was provided more than once".into());
-        }
-        if tg.is_some() {
-            self.target_dir = tg;
-        } else {
-            return Err("The argument '--target-dir' require a value".into());
-        }
-        Ok(())
-    }
-
-    //build config from argument passed to app
-    fn _args(mut self) -> Result<Self, DynError> {
+impl<'a> Config<'a> {
+    //build config from environment variable and passed argument
+    fn from_env() -> Result<Self, DynError> {
         let mut args = env::args();
-        self.subcommand = args.nth(1);
-        while let Some(arg) = args.next() {
-            match arg.as_ref() {
-                "-p" | "--package" => self._arg_package(args.next())?,
-                "--release" => self.release = true,
-                "--target" => self._arg_target(args.next())?,
-                "--target-dir" => self._arg_target_dir(args.next())?,
-                _ => return Err(format!("Unexptected argument: '{}'", arg).into()),
-            }
-        }
-        Ok(self)
-    }
-
-    fn _packages_conf(mut self) -> Result<Self, DynError> {
-        let mut packages_conf = vec![];
-        let mut eg_worker_rs = PackageConf::with_name("eg-worker-rs");
-        eg_worker_rs.add_template_file("manifest.ttl.in");
-        eg_worker_rs.add_template_sub("@LIB_FILE_NAME@", &self.lib_filename("eg-worker-rs")?);
-        packages_conf.push(eg_worker_rs);
-
-        if self.packages.is_empty() {
-            self.packages_conf = packages_conf;
+        let subcommand = if let Some(arg) = args.nth(1) {
+            arg
         } else {
-            for p_name in &self.packages {
-                for p in &packages_conf {
-                    if &p.name == p_name {
-                        self.packages_conf.push(p.clone());
-                    }
+            String::from("")
+        };
+
+        let mut opts_args = Vec::<String>::new();
+        for e in args {
+            if e == "--" {
+                break;
+            }
+            opts_args.push(e);
+        }
+
+        let mut opts = Options::new();
+        opts.optmulti("p", "project", "project to build", "NAME");
+        opts.optflag("", "release", "build in release mode, with optimization");
+        opts.optopt("", "target", "build for the target triple", "TRIPLE");
+        opts.optopt(
+            "",
+            "target-dir",
+            "directory for all generated artifacts",
+            "DIRECTORY",
+        );
+        opts.optflag("h", "help", "print this help menu");
+        let matches = opts.parse(&opts_args)?;
+
+        let target = if let Some(s) = matches.opt_str("target") {
+            s
+        } else if let Some(var) = env::var_os("CARGO_BUILD_TARGET") {
+            var.into_string().unwrap()
+        } else {
+            String::from("")
+        };
+
+        let target_dir = if let Some(s) = matches.opt_str("target-dir") {
+            s
+        } else if let Some(var) = env::var_os("CARGO_TARGET_DIR") {
+            var.into_string().unwrap()
+        } else if let Some(var) = env::var_os("CARGO_BUILD_TARGET_DIR") {
+            var.into_string().unwrap()
+        } else {
+            String::from("target")
+        };
+
+        let release = matches.opt_present("release");
+
+        let mut packages_conf = Vec::<PackageConf>::new();
+        let project = matches.opt_strs("p");
+        for proj in project {
+            for pkg_conf in PACKAGES_CONF {
+                if proj == pkg_conf.name {
+                    packages_conf.push(*pkg_conf);
                 }
             }
         }
-        Ok(self)
+
+        Ok(Self {
+            subcommand,
+            target,
+            target_dir,
+            release,
+            packages_conf,
+            opts,
+        })
     }
 
-    //build config from environment variable and passed argument
-    fn from_env() -> Result<Self, DynError> {
-        Ok(Config::_new()._vars()?._args()?._packages_conf()?)
+    fn print_help(&self) {
+        let brief = "Usage: cargo xtask SUBCOMMAND [options]";
+        let mut usage = self.opts.usage(&brief);
+        let subcommand = "
+     Subcomands are:
+         build   build lv2 project(s)
+
+";
+        usage.push_str(&subcommand);
+        print!("{}", usage);
     }
 
     fn build_dir(&self) -> PathBuf {
-        let target_dir = self.target_dir.as_deref().unwrap_or_default();
-        let target = self.target.as_deref().unwrap_or("target");
         let profile_dir = if self.release { "release" } else { "debug" };
         workspace_root()
-            .join(target_dir)
-            .join(target)
+            .join(&self.target_dir)
+            .join(&self.target)
             .join(profile_dir)
     }
 
@@ -160,26 +139,34 @@ impl Config {
         self.packages_conf.clone()
     }
 
-    fn lib_filename(&self, project_name: &str) -> Result<String, DynError> {
-        let (prefix, suffix) = if let Some(tg) = self.target.as_deref() {
-            if tg.contains("apple") {
-                ("lib", ".dylib")
-            } else if tg.contains("windows") {
-                ("", ".dll")
-            } else {
-                ("lib", ".so")
-            }
-        //if no target provided, use xtask target information wich are host information
+    fn lib_prefix(&self) -> String {
+        let prefix = if self.target.contains("apple") {
+            "lib"
+        } else if self.target.contains("windows") {
+            ""
         } else if cfg!(target_vendor = "apple") {
-            ("lib", ".dylib")
+            "lib"
         } else if cfg!(target_os = "windows") {
-            ("", ".dll")
+            ""
         } else {
-            ("lib", ".so")
+            "lib"
         };
+        String::from(prefix)
+    }
 
-        let base_name = project_name.replace("-", "_");
-        Ok([prefix, &base_name, suffix].concat())
+    fn lib_suffix(&self) -> String {
+        let suffix = if self.target.contains("apple") {
+            ".dylib"
+        } else if self.target.contains("windows") {
+            ".dll"
+        } else if cfg!(target_vendor = "apple") {
+            ".dylib"
+        } else if cfg!(target_os = "windows") {
+            ".dll"
+        } else {
+            ".so"
+        };
+        String::from(suffix)
     }
 }
 
@@ -192,44 +179,33 @@ fn main() {
 
 fn try_main() -> Result<(), DynError> {
     let mut conf = Config::from_env()?;
-    if let Some(task) = &conf.subcommand {
-        match task.as_ref() {
-            "build" => build(&mut conf)?,
-            "debug" => debug(&mut conf)?,
-            _ => print_help(),
-        }
-    } else {
-        print_help();
+    match conf.subcommand.as_ref() {
+        "build" => build(&mut conf)?,
+        "debug" => debug(&mut conf)?,
+        _ => conf.print_help(),
     }
     Ok(())
 }
 
-fn print_help() {
-    eprintln!(
-        "Tasks:
-
-build            build lv2 bundle(s)
-"
-    )
-}
-
 fn build(conf: &mut Config) -> Result<(), DynError> {
     let mut cargo_args = Vec::<String>::new();
-    if let Some(target) = conf.target.as_deref() {
+    if conf.target != "" {
         cargo_args.push(String::from("--target"));
-        cargo_args.push(String::from(target));
+        cargo_args.push(conf.target.clone());
     }
-    if let Some(target_dir) = conf.target_dir.as_deref() {
-        cargo_args.push(String::from("--target-dir"));
-        cargo_args.push(String::from(target_dir));
-    }
+    cargo_args.push(String::from("--target-dir"));
+    cargo_args.push(conf.target_dir.clone());
+
     for p in conf.packages_conf() {
         cargo_args.push(String::from("-p"));
-        cargo_args.push(p.name);
+        cargo_args.push(String::from(p.name));
     }
-
+    println!("Building binarie(s)");
     cargo("build", &cargo_args)?;
+    println!("Building template(s)");
     build_templates(conf)?;
+    println!("Copying ressource(s)");
+    build_resources(conf)?;
 
     println!();
 
@@ -239,26 +215,36 @@ fn build(conf: &mut Config) -> Result<(), DynError> {
 fn build_templates(conf: &mut Config) -> Result<(), DynError> {
     for p in conf.packages_conf() {
         let out_dir = conf.build_dir().join("lv2").join(&p.dir);
+        fs::create_dir_all(&out_dir).unwrap();
         for tf in p.template_files {
-            let template_in_path = workspace_root().join(&p.dir).join(&tf);
-            let template_out_path = out_dir.join(Path::new(&tf).file_stem().unwrap());
-            subs(&template_in_path, &template_out_path, &p.template_subs)?;
+            let in_path = workspace_root().join(&p.dir).join(&tf);
+            let out_path = out_dir.join(Path::new(&tf));
+            let mut template = BufReader::new(File::open(in_path).unwrap());
+            let mut output = BufWriter::new(File::create(out_path).unwrap());
+            let mut buf = String::new();
+            while template.read_line(&mut buf).unwrap() != 0 {
+                for (token, value) in p.template_subs {
+                    buf = buf.replace(token, value);
+                }
+                buf = buf.replace(p.prefix_tag, &conf.lib_prefix());
+                buf = buf.replace(p.suffix_tag, &conf.lib_suffix());
+                write!(output, "{}", buf).unwrap();
+                buf.clear();
+            }
         }
     }
     Ok(())
 }
 
-fn subs(template: &Path, output: &Path, subs: &[(String, String)]) -> Result<(), DynError> {
-    fs::create_dir_all(output.parent().unwrap()).unwrap();
-    let mut template = BufReader::new(File::open(template).unwrap());
-    let mut output = BufWriter::new(File::create(output).unwrap());
-    let mut buf = String::new();
-    while template.read_line(&mut buf).unwrap() != 0 {
-        for (token, value) in subs {
-            buf = buf.replace(token, value);
+fn build_resources(conf: &mut Config) -> Result<(), DynError> {
+    for p in conf.packages_conf() {
+        let out_dir = conf.build_dir().join("lv2").join(&p.dir);
+        fs::create_dir_all(&out_dir).unwrap();
+        for rs in p.resources {
+            let in_path = workspace_root().join(&p.dir).join(&rs);
+            let out_path = out_dir.join(Path::new(&rs));
+            fs::copy(in_path, out_path)?;
         }
-        write!(output, "{}", buf).unwrap();
-        buf.clear();
     }
     Ok(())
 }
